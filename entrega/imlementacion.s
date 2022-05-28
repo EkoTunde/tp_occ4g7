686dc94f21c4d99f698be11f0edd84696b208a55
Juan Cruz Maciel Henning <jcmacielhenning@gmail.com>
2022-05-28 12:35:37 -0300

    se agregaron los archivos de pseudocodigo

--- entrega/imlementacion.s
+++ entrega/imlementacion.s
@@ -1,16 +1,20 @@
+
+@ Declaramos las constantes, reservando el espacio en memoria
 .data
     @Todo
 
 
+@ Declaramos nuestro código del programa
 .text
     @Todo
 
-
+    @ Donde empieza nuestro programa
     .global main
     main:
         @Todo
 
 
+    @ Para manejar el fin del programa
     fin:
         mov r7, #1
         swi 0
---
+++ pseudo/pseudocod.java
@@ -0,0 +1,29 @@
+package pseudo;
+
+class Pseudo {
+
+    private static void imprimir() {
+        System.out.println("Hola");
+    }
+
+    private static void imprimirMapa() {
+        // Todo
+    }
+
+    private static void imprimirAciertos() {
+        // Todo
+    }
+
+    private static void imprimirInfo() {
+        // Todo
+    }
+
+    private static void escanearLetra() {
+        // Todo
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hola");
+    }
+
+}
