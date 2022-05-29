f71b492e6b0214d90dd888978a260e060b9371e4
Juan Cruz Maciel Henning <jcmacielhenning@gmail.com>
2022-05-29 16:13:49 -0300

    actualizo codigo entrega con funciones imprimirFeedback funcionando

--- entrega/entrega.s
+++ entrega/entrega.s
@@ -1,14 +1,28 @@
 
 @ Declaramos las constantes, reservando el espacio en memoria
 .data
-    @Todo
+    feedback: .asciz "Quedan x letras por adivinar."
+    vidas: .byte 7
+	vida0: .ascii "0"
 
 
 @ Declaramos nuestro código del programa
 .text
 
-    // Reemplaza el caracter de la posicion especificada
-    // por otro en un vector asciz (termina en nulo)
+    // Retorna un numero en su equivalente de ascii
+    // Entrada r0: el numero en ascii
+    // Salida r1: el equivalente de ascii
+    asciiDeNum:
+        .fnstart
+      		push {r0, lr}
+      		ldr r1, =vida0
+      		ldrb r1, [r1]
+      		add r1, r0
+      		pop {r0, lr}
+      		bx lr
+        .fnend
+
+    // Reemplaza el caracter de la posicion especificada por otro
     // Entrada r1: dirección de memoria del vector
     // Entrada r2: posicion del caracter a reemplazar
     // Entrada r3: caracter nuevo
@@ -16,24 +30,26 @@
         .fnstart
             push {r1, r2, r3, lr}   // Protegemos registros
             add r1, r2              // Sumo la posición deseada a la dirección que apunta mi vector
-            str r3, r1              // Cargamos lo que está en r3 en la dirección de r1
+            strb r3, [r1]              // Cargamos lo que está en r3 en la dirección de r1
             pop {r1, r2, r3, lr}    // Quitamos protección
             bx lr                   // Volvemos
         .fnend
 
-    // Entradas: r1 (dirección de memoria), r2 (tamaño cadena)
+    // Entrada r1 -> dirección de memoria
+    // Entrada r2 -> tamaño cadena
     imprimir:
         .fnstart
-        push {r0, lr}
-        mov r7, #4  // La función de swi que necesitamos para imprimir
-        mov r0, #1
-        swi 0
-        pop {r0, lr}
+            push {r0, r7, lr}
+            mov r7, #4  // La función de swi que necesitamos para imprimir
+            mov r0, #1
+            swi 0
+            pop {r0, r7, lr}
+            bx lr
         .fnend
 
     // Retorna el largo de la cadena
-    // Entradas: r1 (dirección de memoria)
-    // Saida: r0 (longitud cadena)
+    // Entradas r1: dirección de memoria
+    // Saida r0: longitud cadena
     largoCadena:
         .fnstart
             push {r1, lr}
@@ -53,6 +69,21 @@
                 bx lr
         .fnend
 
+    
+    imprimirFeedback:
+        push {r0, r1, r2, r3, lr}   // Protegemos los registros
+        mov r2, #7                  // El índice que queremos reemplazar
+        ldr r6, =vidas              // Direc de las vidas
+        ldr r0, [r6]                // El número de vidas a r0
+        bl asciiDeNum               // En r1 ponemos el equivalente en ascii de r0
+        mov r3, r1                  // Copias el ascii en r3
+        ldr r1, =feedback           // Dirección de memoria de la cadena
+        bl reemplazar               // Reemplazamos con el valor actual de las vidas
+        bl largoCadena              // Obtenemos el largo de la cadena
+        mov r2, r0                  // Guardamos el largo en r2
+        bl imprimir                 // Imprime el mensaje de feedback
+        pop {r0, r1, r2, r3, lr}    // Quitamos la protección de los registros
+        bx lr                       // Volvemos
 
     @ Donde empieza nuestro programa
     .global main
@@ -62,5 +93,5 @@
 
     @ Para manejar el fin del programa
     fin:
-        mov r7, #1
-        swi 0
+        mov r7, #1  // Instrucción para salir del programa
+        swi 0       // Interrumpimos para terminas
