6ae3475447bf1f58562243af75cb4a9c01b9d0cf
Juan Cruz Maciel Henning <jcmacielhenning@gmail.com>
2022-06-22 23:57:01 -0300

    todo hasta el 22 junio

---
+++ jun22/preguntas.py
@@ -0,0 +1,114 @@
+PREGUNTAS = {
+    1: {
+        'preg': '¿Cuántos metros de altura tiene la gran pirámide de Guiza?',
+        'rta': 138,
+        'maxRta': 140,
+        'minRta': 130,
+    },
+    2: {
+        'preg': '¿Cuántos años luz de distancia nos separan de la galaxia Andrómeda, la más cercana a la vía láctea??',
+        'rta': 2000000,
+        'maxRta': 2500000,
+        'minRta': 1500000,
+    },
+    3: {
+        'preg': '¿Cuántos segundos tarda la luz del sol en llegar a la tierra?',
+        'rta': 489,
+        'maxRta': 500,
+        'minRta': 400,
+    },
+    4: {
+        'preg': '¿Cuántos billones de habitantes hay en la Tierra en 2022?',
+        'rta': 8,
+        'maxRta': 9,
+        'minRta': 7,
+    },
+    5: {
+        'preg': '¿Cuántos millones de bitcoins pueden existir?',
+        'rta': 21,
+        'maxRta': 25,
+        'minRta': 20,
+    },
+    6: {
+        'preg': '¿Cuántos millones de bitcoins pueden existir?',
+        'rta': 21,
+        'maxRta': 25,
+        'minRta': 20,
+    },
+}
+
+
+preg6: .asciz "¿Cuándo acabó la II Guerra Mundial?" // 6
+rta6: .word 1945
+maxRta6: .word 1946
+minRta6: .word 1944
+
+preg7: .asciz "¿En qué año llegó Cristóbal Colón a América?" // 7
+rta7: .word 1492
+maxRta7: .word 1500
+minRta7: .word 1490
+
+preg8: .asciz "¿Cuántas millones de copias vendió el disco Thriller, de Michael Jackson, el más vendido de la historia?" // 8
+rta8: .word 65
+maxRta8: .word 70
+minRta8: .word 60
+
+preg9: .asciz "¿Cuántos km2 mide Rusia, el país más grande del mundo? Psst: son varios millones ;)" // 9
+rta9: .word 17075200
+maxRta9: .word 18000000
+minRta9: .word 17000000
+
+preg10: .asciz "¿De cuántos miles de millones de USD fue el PBI de Argentina al 2020?" // 10
+rta10: .word 383
+maxRta10: .word 400
+minRta10: .word 350
+
+preg11: .asciz "¿Cuántos miles de millones de habitantes posee China, el país más poblado de la Tierra?" // 11
+rta11: .word 1402
+maxRta11: .word 1450
+minRta11: .word 1350
+
+preg12: .asciz "¿Cuántos números primos hay entre el 1 y el 100?" // 12
+rta12: .word 25
+maxRta12: .word 27
+minRta12: .word 23
+
+preg13: .asciz "¿Cuántos átomos en fila caben en el diámetro de un cabello humano (unas 80 micras)?" // 13
+rta13: .word 800000
+maxRta13: .word 27
+minRta13: .word 23
+
+preg14: .asciz "¿Cuál es el número atómico del Europio (Eu)?" // 14
+rta14: .word 63
+maxRta14: .word 65
+minRta14: .word 60
+
+preg15: .asciz "¿Cuántos km mide el Amazonas?" // 15
+rta15: .word 6992
+maxRta15: .word 7000
+minRta15: .word 6800
+
+preg16: .asciz "¿Cuánto vale 'pi' en sus 7 primeras posiciones detrás de la coma?" // 16
+rta16: .word 1415926
+maxRta16: .word 1500000
+minRta16: .word 1400000
+
+preg17: .asciz "¿A cuántos hercios (Hz) está afinada la tecla A4, o 'la 440'?" // 17
+rta17: .word 440000
+maxRta17: .word 450000
+minRta17: .word 400000
+
+preg18: .asciz "¿En qué año apareció por primera vez en un videojuego Jumpman, quien luego sería conocido como Super Mario?" // 18
+rta18: .word 1981
+maxRta18: .word 1985
+minRta18: .word 1980
+
+preg19: .asciz "¿Qué porcentaje de la población de Camboya es budista, el país con mayor densidad poblacional con estas características?" // 20
+rta19: .word 90
+maxRta19: .word 95
+minRta19: .word 85
+
+preg20: .asciz "¿En qué año se produce la Revolución Francesa?"
+rta20: .word 1789
+maxRta20: .word 1785
+minRta20: .word 1790
